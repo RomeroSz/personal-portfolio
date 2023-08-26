cc8369117c31514118e837484841e1bbbba431f2
Victor Romero <victoromero2505@gmail.com>
2023-08-26 05:04:39 -0400

    First stable version of the project. Starting to divide the program into components. Still need to fix the navbar button issue and decide whether to add a scroll to up and autoscroll to specific sections.

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRouting } from './home.routing';
 import { HomeComponent } from './home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -11,7 +12,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRouting
+    HomeRouting,
+    NgbModule,
   ]
 })
 export class HomeModule { }
